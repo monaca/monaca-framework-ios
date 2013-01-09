b5f880d3c8ede1e4d5bebb6a1760ee1524b99d07
katsuya <katsuya@asial.co.jp>
2013-01-09 20:57:55 +0900

    Added urlEncod test

--- MFUnitTest/MFUtilityTest.m
+++ MFUnitTest/MFUtilityTest.m
@@ -12,12 +12,13 @@
 
 - (void)testEncodeURIComponent
 {
-/*    GHAssertEqualStrings([MDUtility encodeURIComponent:@"test@monaca.mobi"], @"test%40monaca.mobi", nil);
-    GHAssertEqualStrings([MDUtility encodeURIComponent:@"monaca+mobi"], @"monaca%2Bmobi", nil);
-    GHAssertEqualStrings([MDUtility encodeURIComponent:@"?hoge=piyo"], @"%3Fhoge%3Dpiyo", nil);
-    GHAssertEqualStrings([MDUtility encodeURIComponent:@"?hoge=piyo&foo=bar"], @"%3Fhoge%3Dpiyo%26foo%3Dbar", nil);
-    GHAssertEqualStrings([MDUtility encodeURIComponent:@"!*'();:@&=+$,/?%#[]"], @"%21%2A%27%28%29%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23%5B%5D", nil);
- */
+    GHAssertEqualStrings([MFUtility urlEncode:@"test@monaca.mobi"], @"test%40monaca.mobi", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:@"monaca+mobi"], @"monaca%2Bmobi", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:@"?hoge=piyo"], @"%3Fhoge%3Dpiyo", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:@"?hoge=piyo&foo=bar"], @"%3Fhoge%3Dpiyo%26foo%3Dbar", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:@"!*'();:@&=+$,/?%#[]"], @"%21%2A%27%28%29%3B%3A%40%26%3D%2B%24%2C%2F%3F%25%23%5B%5D", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:0], @"", nil);
+    GHAssertEqualStrings([MFUtility urlEncode:[NSArray array]], @"", nil);
 }
 
 @end
