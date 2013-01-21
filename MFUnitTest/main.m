9c4ffbcd71d22de842d603f57a7f6ffd92593ae6
Katsuya Saito <katsuya@asial.co.jp>
2013-01-21 18:00:23 +0900

    Fixed unit test style

--- MFUnitTest/main.m
+++ MFUnitTest/main.m
@@ -11,7 +11,12 @@
 
 int main(int argc, char *argv[])
 {
+    int retVal;
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+        if (getenv("GHUNIT_CLI")) {
+            retVal = [GHTestRunner run];
+        } else {
+            retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+        }
     }
 }
