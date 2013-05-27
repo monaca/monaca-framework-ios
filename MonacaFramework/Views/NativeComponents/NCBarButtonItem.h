3efc34fae51b77aa3525911ac333acec087b03be
shikatah <shikatah@asial.co.jp>
2013-05-27 12:50:15 +0900

    Ticket 8041&8091 fixed.

---
+++ MonacaFramework/Views/NativeComponents/NCBarButtonItem.h
@@ -0,0 +1,17 @@
+//
+//  NCBarButtonItem.h
+//  MonacaFramework
+//
+//  Created by Shikata on 4/25/13.
+//  Copyright (c) 2013 ASIAL CORPORATION. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NCBarButtonItem : UIBarButtonItem {
+    BOOL hidden_;
+}
+
+@property(nonatomic,assign)BOOL hidden;
+
+@end
