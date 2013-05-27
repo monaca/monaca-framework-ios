aacf6d8b78e47aaf748d53a6d64d1287733b1bf2
yasu <arte_lprk@hotmail.co.jp>
2013-05-28 02:26:25 +0900

    More Fixed naviBar hide timing

--- MonacaFramework/Library/Monaca/Transit/MFTransitPlugin.m
+++ MonacaFramework/Library/Monaca/Transit/MFTransitPlugin.m
@@ -77,7 +77,6 @@
 
     MFNavigationController *navigationController;
     if ([parameter.target isEqualToString:@"_parent"]) {
-        [[MFUtility getAppDelegate].monacaNavigationController setNavigationBarHidden:YES];
         navigationController = [MFUtility getAppDelegate].monacaNavigationController;
     } else {
         [MFViewBuilder setIgnoreBottom:YES];
