28599762fcf8a9d006b098569400ee606fa6e83d
shikatah <shikatah@asial.co.jp>
2013-07-09 17:45:34 +0900

    remove IPP. Add MonacaBackend Plugin.

---
+++ MonacaFramework/Library/Monaca/Device/MFDevicePlugin.h
@@ -0,0 +1,13 @@
+//
+//  MFDevice.h
+//  MonacaFramework
+//
+//  Created by Shikata on 7/5/13.
+//  Copyright (c) 2013 ASIAL CORPORATION. All rights reserved.
+//
+
+#import "CDVPlugin.h"
+
+@interface MFDevicePlugin : CDVPlugin
+
+@end
