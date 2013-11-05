f9289e81af257f1f09afa9a43dfabf475440f521
shikatah <shikatah@asial.co.jp>
2013-11-05 16:46:33 +0900

    fix backButton default color.

--- MonacaFramework/Views/NativeComponents/NCStyle.m
+++ MonacaFramework/Views/NativeComponents/NCStyle.m
@@ -89,8 +89,14 @@
         [defaultStyle setValue:kNCFalse forKey:kNCStyleDisable];
         [defaultStyle setValue:kNCUndefined forKey:kNCStyleImage];
         [defaultStyle setValue:kNCUndefined forKey:kNCStyleText];
-        [defaultStyle setValue:kNCStyleIos7DefaultBlue forKey:kNCStyleTextColor];
 
+#ifdef XCODE5
+        [defaultStyle setValue:kNCStyleIos7DefaultBlue forKey:kNCStyleTextColor];
+#else
+        [defaultStyle setValue:kNCWhite forKey:kNCStyleTextColor];
+#endif
+        
+        
 #ifdef XCODE5
         // iOS6以下の場合の設定
         if ([MFDevice iOSVersionMajor] <= 6) {
@@ -109,8 +115,14 @@
     if ([component isEqualToString:kNCComponentBackButton]) {
         [defaultStyle setValue:kNCTrue forKey:kNCStyleVisibility];
         [defaultStyle setValue:kNCFalse forKey:kNCStyleDisable];
+        
+#ifdef XCODE5
         [defaultStyle setValue:kNCStyleIos7DefaultBlue forKey:kNCStyleText];
-
+#else
+        [defaultStyle setValue:kNCWhite forKey:kNCStyleText];
+#endif
+        
+        
 #ifdef XCODE5
         // iOS6以下の場合の設定
         if ([MFDevice iOSVersionMajor] <= 6) {
